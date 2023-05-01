58e9cf226080c7f7cd0f8475d4e87c2fc928f21f
melrothiago <melrothiago@gmail.com>
2023-05-01 17:11:39 -0300

    added a default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Button } from './components/Button.tsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './components/styles/themes/default.ts'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary' />
       <Button variant='secondary' />
       <Button variant='success' />
       <Button variant='danger' />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }

2b412e46b20badee36a2855a07373675c19db977
melrothiago <melrothiago@gmail.com>
2023-05-01 18:10:08 -0300

    created type definition

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -6,12 +6,12 @@ interface ButtonContainerProps {
     variant: ButtonVariant;
 }
 
-const buttonVariants = {
+/*const buttonVariants = {
     primary: 'purple',
     secondary: 'orange',
     danger: 'red',
     success: 'green'
-}
+}*/
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;

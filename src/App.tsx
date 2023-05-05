eda852a6a16bf24553c4acd3ffb6d837bdc7cf92
melrothiago <melrothiago@gmail.com>
2023-05-05 14:45:26 -0300

    creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Button } from './components/Button.tsx'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './components/styles/Router.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './components/styles/themes/default.ts'
 import { GlobalStyle } from './components/styles/global.ts'
@@ -7,12 +8,9 @@ import { GlobalStyle } from './components/styles/global.ts'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
